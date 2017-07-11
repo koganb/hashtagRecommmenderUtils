968a405420e42b38c4bb84ca68495fa9c2bd3d30
Boris <siboris@hotmail.com>
2017-07-11 11:16:46 +0300

    fix merged file creation

--- twitter2inputFile.py
+++ twitter2inputFile.py
@@ -1,5 +1,4 @@
-
-#using as example https://github.com/yanxurui/csv2es/blob/master/csv2es.py
+# using as example https://github.com/yanxurui/csv2es/blob/master/csv2es.py
 import json
 import urllib2
 from urllib2 import *
@@ -7,7 +6,9 @@ import click
 import dateutil.parser
 import warnings
 import random
+
 warnings.filterwarnings("ignore", category=UnicodeWarning)
+
 
 def transform_json(line):
     j = json.loads(line)
@@ -20,26 +21,38 @@ def transform_json(line):
     return '"%s";"%s";"%s";"%s";""\n' % (user_id, tweet_id, timestamp, hash_tags)
 
 
-def read (infile, outfile):
+def read(infile, outfile):
+    train_file_name = outfile + "_train.txt"
+    test_file_name = outfile + "_test.txt"
+    train_test_file_name = outfile + ".txt"
+
     with open(infile) as fin:
-        with open(outfile + ".txt",'w') as fout:
-            with open(outfile + "_train.txt",'w') as fout_train:
-                with open(outfile + "_test.txt",'w') as fout_test:
-                    for line in fin:
-                        if len(line.strip()) > 0:
-                            line = transform_json(line)
-                            fout.write(line)
-                            if random.randint(0, 4) == 0:
-                                fout_test.write(line)
-                            else:
-                                fout_train.write(line)
+        with open(train_file_name, 'w') as fout_train:
+            with open(test_file_name, 'w') as fout_test:
+                for line in fin:
+                    if len(line.strip()) > 0:
+                        line = transform_json(line)
+                        if random.randint(0, 4) == 0:
+                            fout_test.write(line)
+                        else:
+                            fout_train.write(line)
+
+    import fileinput
+    with open(train_test_file_name, 'w') as fout:
+        fin = fileinput.input([train_file_name, test_file_name])
+        for line in fin:
+            fout.write(line)
+        fin.close()
+
+
 
 @click.command()
 @click.argument('infile', required=True)
 @click.argument('outfile', required=True)
 @click.pass_context
-def cli(ctx, infile, outfile,**opts):
+def cli(ctx, infile, outfile, **opts):
     read(infile, outfile)
+
 
 if __name__ == '__main__':
     cli()
